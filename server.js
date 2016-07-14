31779833ea54c49d528a7ea3b5672a1bc6e914da
Selçuk Oğuz <selcuk.oguz@hotmail.com.tr>
2016-07-14 20:22:56 +0300

    add todo update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3000;
+var todoNextId = 1;
 var todos = [{
 	id: 1,
 	description: "Meet mom for lunch",
@@ -12,12 +14,17 @@ var todos = [{
 }];
 
 app
+
+	.use(bodyParser.json()) //middleware
+
 	.get('/', function(req, res){
 		res.send('Todo API Root');
 	})
+
 	.get('/todos', function(req, res){
 		res.json(todos);
 	})
+
 	.get('/todos/:id', function(req, res){
 		var id = parseInt(req.params.id, 10);
 
@@ -29,6 +36,19 @@ app
 
 		res.status(404).send();
 	})
+
+	.post('/todos', function(req, res){
+		var body = req.body;
+		body.id = addAnId();
+		body.completed = false;
+		todos.push(body);
+		res.json(body)
+	})
+
 	.listen(port, function(){
 		console.log('Express listening on', port, ' !');
 	});
+
+	function addAnId(){
+		return parseInt(todos[todos.length-1].id) + 1;
+	}
