1a41925e29a0aeef9ce60b064598d1121fdaf997
Selçuk Oğuz <selcuk.oguz@hotmail.com.tr>
2016-07-21 22:46:20 +0300

    Over project

--- server.js
+++ server.js
@@ -156,21 +156,30 @@ app.post('/users', function(req, res){
 
 app.post('/users/login', function(req, res){
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.auth(body).then(function(user){
 		var token = user.generateToken('authentication');
+		userInstance = user
+		return db.token.create({
+			token: token
+		});
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		}else{
-			res.status(401).send();
-		}
-	}, function(e){
+	}).then(function(token){
+		res.header('Auth', token.get('token')).json(userInstance.toPublicJSON());
+	}).catch( function(e){
 		res.status(401).send();
 	});
 });
 
 
+app.delete('/users/login', middleware.requireAuthentication, function(req, res){
+	req.token.destroy().then(function(){
+		res.status(204).send();
+	}, function(){
+		res.status(500).send();
+	})
+});
 
 db.sequelize.sync({force:true}).then(function() {
 	app.listen(port, function() {
